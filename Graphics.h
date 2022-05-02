e634dd26d8b12d9585be3601bb17ca5f8be14463
James <jamesw.0360@gmail.com>
2022-05-02 16:38:47 +1000

    Rendering now works by drawing to the back buffer of a swap chain, then presenting the swap chain after drawing has finished.

--- Graphics.h
+++ Graphics.h
@@ -27,7 +27,7 @@ class Graphics
 	ID2D1Factory1* pD2DFactory;
 	ID2D1Device* pD2DDevice;
 	ID2D1DeviceContext* pD2DContext;
-	ID2D1Bitmap1* pBackBuffer;
+	ID2D1Bitmap1* pDrawingTarget;
 
 	// WIC
 	IWICImagingFactory* pImagingFactory;
@@ -51,6 +51,8 @@ public:
 
 	void Setup(HWND hWnd)
 	{
+		windowDPI = static_cast<float>(GetDpiForWindow(hWnd));
+
 		HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
 		if (SUCCEEDED(hr))
 		{
@@ -64,7 +66,6 @@ public:
 
 		if (SUCCEEDED(hr))
 		{
-			/**/
 			// Create the D3D device.
 			hr = D3D11CreateDevice(
 				NULL,
@@ -92,15 +93,29 @@ public:
 
 			// Get the factory to create a swap chain.
 			IDXGIAdapter* pAdapter;
-			pDXGIDevice->GetAdapter(&pAdapter);
-			IDXGIFactory* pFactory;
-			pAdapter->GetParent(IID_PPV_ARGS(&pFactory));
+			hr = pDXGIDevice->GetAdapter(&pAdapter);
+			IDXGIFactory2* pFactory;
+			hr = pAdapter->GetParent(IID_PPV_ARGS(&pFactory));
 
 			// Description for the swapchain.
 			DXGI_SWAP_CHAIN_DESC1 swapDesc;
+			swapDesc.Width = 0;                           // use automatic sizing
+			swapDesc.Height = 0;
+			swapDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; // this is the most common swapchain format
+			swapDesc.Stereo = false;
+			swapDesc.SampleDesc.Count = 1;                // don't use multi-sampling
+			swapDesc.SampleDesc.Quality = 0;
+			swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+			swapDesc.BufferCount = 2;                     // use double buffering to enable flip
+			swapDesc.Scaling = DXGI_SCALING_NONE;
+			swapDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL; // all apps must use this SwapEffect
+			swapDesc.Flags = 0;
+			swapDesc.AlphaMode = DXGI_ALPHA_MODE_IGNORE;
 
+
+			// DXGI_ERROR_INVALID_CALL 0x887A0001
 			// Create swap chain for hWnd.
-			reinterpret_cast<IDXGIFactory2*>(pFactory)->CreateSwapChainForHwnd(
+			hr = pFactory->CreateSwapChainForHwnd(
 				pDXGIDevice,
 				hWnd,
 				&swapDesc,
@@ -109,6 +124,21 @@ public:
 				&pDXGISwapChain
 			);
 
+			// Get the back buffer of the swap chain.
+			IDXGISurface* pBackBuffer;
+			hr = pDXGISwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+
+			// Create a bitmap from the surface so Direct 2D can draw to it.
+			D2D1_BITMAP_PROPERTIES1 bitmapProperties;
+			bitmapProperties.pixelFormat = D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE);
+			bitmapProperties.dpiX = windowDPI;
+			bitmapProperties.dpiY = windowDPI;
+			bitmapProperties.bitmapOptions = D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW; // why cannot draw tag
+			bitmapProperties.colorContext = NULL;
+			hr = pD2DContext->CreateBitmapFromDxgiSurface(pBackBuffer, bitmapProperties, &pDrawingTarget);
+
+			// Make the context render to the drawing target.
+			pD2DContext->SetTarget(pDrawingTarget);
 
 		}
 	}
